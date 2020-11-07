b8ff299373cef7d0382a8cf8e2318b43d97cd9e1
Peter Rosberg <peter.rosberg@gmail.com>
2020-11-07 16:46:29 +0100

    restart initial commit

---
+++ src/main/kotlin/com/pepp/wordreversal/WordReversal/WordReversalApplication.kt
@@ -0,0 +1,11 @@
+package com.pepp.wordreversal.WordReversal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WordReversalApplication
+
+fun main(args: Array<String>) {
+	runApplication<WordReversalApplication>(*args)
+}
---
+++ src/test/kotlin/com/pepp/wordreversal/WordReversal/WordReversalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pepp.wordreversal.WordReversal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WordReversalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
