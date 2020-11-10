da9b69c4ca1fc1254180d5a5c9e7eb019d6b10d0
Peter Rosberg <peter.rosberg@gmail.com>
2020-11-10 20:42:21 +0100

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,12 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
