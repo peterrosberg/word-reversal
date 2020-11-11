f2bbe3084bb4653cee368049e48227a250beb63a
Peter Rosberg <peter.rosberg@gmail.com>
2020-11-12 00:51:19 +0100

    get latest backend call in GUI

--- frontend/src/backend.js
+++ frontend/src/backend.js
@@ -19,3 +19,24 @@ export async function postSentence(sentence) {
         throw new Error('Could not post sentence for reversal!\n' + error.message);
     }
 }
+
+export async function getLatest() {
+    try {
+        const response = await fetch('/api/latest', {
+            headers: {
+                Accept: 'application/json'
+            },
+            method: 'GET'
+        });
+        //TODO: Clear logs
+        console.log(response);
+        if (!response.ok) {
+            return { error: "Error " + response.status };
+        }
+        const responseJson = await response.json();
+        console.log(responseJson);
+        return responseJson;
+    } catch (error) {
+        throw new Error('Could not get latest reversals!\n' + error.message);
+    }
+}
