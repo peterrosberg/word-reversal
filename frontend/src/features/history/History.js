5b2a90c1ae2614b09d225b659749703fabeee79e
Peter Rosberg <peter.rosberg@gmail.com>
2020-11-15 22:51:51 +0100

    Trigger periodic updates of History

--- frontend/src/features/history/History.js
+++ frontend/src/features/history/History.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
     history,
     isLoaded,
     isError,
-    errorMessage
+    errorMessage,
+    loadHistory
 } from './historySlice';
 import moment from 'moment';
 import styles from './History.module.css';
 
 export function History() {
+    const dispatch = useDispatch();
     const error = useSelector(isError);
     const loaded = useSelector(isLoaded);
     const errorText = useSelector(errorMessage);
     const historyList = useSelector(history);
+
+    useEffect(() => {
+        const timer = setTimeout(
+            () => dispatch(loadHistory()),
+            15000
+        );
+        return () => clearTimeout(timer);
+    });
 
     if (!loaded) {
         return <div id="latestSentences" className={styles.loading}>Loading history...</div>
